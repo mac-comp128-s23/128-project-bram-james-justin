936c25bf50eb30e26724fbe0f7e445850efbabcb
JustinChalichemala <jchalich@macalester.edu>
2023-04-08 17:39:57 -0500

    safety push --> "Bram" placement sort of works!
    changed matrix type

--- src/Board.java
+++ src/Board.java
@@ -1,25 +1,64 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.awt.Color;
 
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Ellipse;
+import edu.macalester.graphics.Fillable;
 import edu.macalester.graphics.GraphicsObject;
+import edu.macalester.graphics.Rectangle;
 
 public class Board {
-    public int[][] gameBoard;
+    public Fillable[][] gameBoard;
+    public int xBoxMargin, yBoxMargin;
+    public int squareHeightAndWidth;
 
     public Board() {
-        gameBoard = new int[7][6];
-        initializeBoard();
+        gameBoard = new Fillable[7][6];
+        xBoxMargin = 100;
+        yBoxMargin = 80;
+        squareHeightAndWidth = 70;
     }
 
-    public void initializeBoard(){
-        for (int[] col : gameBoard) {
-            for (int i : col) {
-                col[i] = 0;
+    // public void initializeBoard(){
+    //     for (GraphicsObject[] col : gameBoard) {
+    //         for (GraphicsObject i : col) {
+    //         }
+    //     }
+    // }
+
+
+    public int getNearestColIndex(double mouseX, double mouseY){ //gets the position of the mouse and then assigns it to the nearest column
+        int answer = -1;
+        if(mouseX > xBoxMargin && mouseX < (squareHeightAndWidth * 7) + xBoxMargin) {
+            answer = (int) ((mouseX - xBoxMargin) / 7); //If something goes wrong check here          
+        }
+        return answer;
+    }
+
+    public void initializePieces(CanvasWindow canvas){   //1 is red, 2 is yellow
+        int colCount = 0;
+        int rowCount = 0;
+        for (Fillable[] col : gameBoard) {
+            rowCount = 0;
+            for (Fillable var: col) {
+                Fillable square= new Rectangle(xBoxMargin + (70 * (colCount % 7)), yBoxMargin + (70 * (rowCount )), squareHeightAndWidth, squareHeightAndWidth);
+                Fillable disc = new Ellipse(105 + (70 * (colCount % 7)), 85 + (70 * (rowCount )), 60, 60);
+                ((Ellipse) disc).setFillColor(Color.WHITE); //If we could set this to translucent that would be cool then we could have the sliding effect later.
+                ((Rectangle) square).setFillColor(Color.BLUE);
+                
+                canvas.add((GraphicsObject) square);
+                canvas.add((GraphicsObject) disc);
+                System.out.println();
+                // ellipseGameBoard[colCount][rowCount] = disc;
+
+                rowCount ++;
             }
+            colCount++;
         }
     }
 
-    public int[][] getGameBoard() {
+    public Fillable[][] getGameBoard() {
         return gameBoard;
     }
 
--- src/GameManager.java
+++ src/GameManager.java
@@ -1,8 +1,11 @@
 import java.awt.Color;
+import java.awt.Paint;
 import java.lang.reflect.Array;
+import java.util.HashMap;
 
 import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.Ellipse;
+import edu.macalester.graphics.Fillable;
 import edu.macalester.graphics.GraphicsGroup;
 import edu.macalester.graphics.GraphicsObject;
 import edu.macalester.graphics.Rectangle;
@@ -13,48 +16,69 @@ public class GameManager {
     private final int CANVAS_WIDTH = 700;
     private final int CANVAS_HEIGHT = 600; 
     private GraphicsGroup pieces;
-    private Integer playerTurn;  //Red if 1, Yellow if 2
+    private boolean player1Turn;  //Red if 1, Yellow if 2
     private GraphicsObject [][] ellipseGameBoard;
+    private HashMap<Integer[][], GraphicsObject> tokenMap;
+    private int turnCount;
     
     public GameManager() {
+        turnCount = 0;
         board = new Board();
         canvas = new CanvasWindow("Board", CANVAS_WIDTH, CANVAS_HEIGHT);
         pieces = new GraphicsGroup();
-        initializePieces(board.getGameBoard());
         canvas.add(pieces, 0, 0);
+        board.initializePieces(canvas);
     }
 
     public void displayBoard(){
 
     }
 
-    public void initializePieces(int[][] gameBoard){   //1 is red, 2 is yellow
-        int colCount = 0;
-        int rowCount = 0;
-        for (int[] col : gameBoard) {
-            rowCount = 0;
-            for (int var: col) {
-                GraphicsObject square= new Rectangle(100 + (70 * (colCount % 7)), 80 + (70 * (rowCount )), 70, 70);
-                GraphicsObject disc = new Ellipse(105 + (70 * (colCount % 7)), 85 + (70 * (rowCount )), 60, 60);
-                ellipseGameBoard[colCount][rowCount]=disc;
-                if(var == 0) {
-                    ((Ellipse) disc).setFillColor(Color.WHITE); //If we could set this to translucent that would be cool then we could have the sliding effect later.
+    // public void initializePieces(int[][] gameBoard){   //1 is red, 2 is yellow
+    //     int colCount = 0;
+    //     int rowCount = 0;
+    //     for (int[] col : gameBoard) {
+    //         rowCount = 0;
+    //         for (int var: col) {
+    //             GraphicsObject square= new Rectangle(100 + (70 * (colCount % 7)), 80 + (70 * (rowCount )), 70, 70);
+    //             GraphicsObject disc = new Ellipse(105 + (70 * (colCount % 7)), 85 + (70 * (rowCount )), 60, 60);
+    //             if(var == 0) {
+    //                 ((Ellipse) disc).setFillColor(Color.WHITE); //If we could set this to translucent that would be cool then we could have the sliding effect later.
+    //             }
+    //             ((Rectangle) square).setFillColor(Color.BLUE);
+    //             canvas.add(square);
+    //             canvas.add(disc);
+    //             // ellipseGameBoard[colCount][rowCount] = disc;
+
+    //             rowCount ++;
+    //         }
+    //         colCount++;
+    //     }
+    // }
+
+
+    // public Ellipse getToken() {
+    //     return pieces;
+    // }   
+
+    public void placePiece(){
+        canvas.onClick(event -> {
+            int index = board.getNearestColIndex(event.getPosition().getX(), event.getPosition().getY());
+            Fillable[] col = board.getGameBoard()[index];
+            Boolean finished = false;
+            int count = 0;
+            if(turnCount < 43){
+                while(count < ){
+                    if(col[count].getFillColor() != Color.WHITE){
+                        board.getGameBoard()[index][count - 1].setFillColor(getPlayerColor());
+                        finished = true;
+                    }
+                    System.out.println("YYYAt");
+                    count ++;
                 }
-                ((Rectangle) square).setFillColor(Color.BLUE);
-                canvas.add(square);
-                canvas.add(disc);
-                rowCount ++;
             }
-            colCount++;
-        }
-    }
-
-
-
-
-    public void placeRed(){
-        canvas.onMouseDown(event -> {
-            ellipseGameBoard[]
+            canvas.draw();
+            player1Turn = !player1Turn;
         });
     }
     // public void placeYellow(){
@@ -65,6 +89,16 @@ public class GameManager {
 // } else {
 //     ((Ellipse) disc).setFillColor(Color.YELLOW);
 // }
+
+    private Paint getPlayerColor() {
+        Paint color;
+        if(player1Turn) {
+            color = Color.RED;
+        } else {
+            color = Color.YELLOW;
+        }
+        return color;
+    }
 
     public static void main(String[] args) {
         GameManager game = new GameManager();
