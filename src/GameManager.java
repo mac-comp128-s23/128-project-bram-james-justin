82e7b46d47e7b2fc3bba38e4f31152b20134f277
JustinChalichemala <jchalich@macalester.edu>
2023-04-09 16:18:59 -0500

    quick Bram save
    System.Out.println("game works! normal connect four is online. We were able to rewrite the conditionals no thanks to bram XD");
    bram==!isCodeGod;

--- src/GameManager.java
+++ src/GameManager.java
@@ -16,83 +16,116 @@ public class GameManager {
     private final int CANVAS_WIDTH = 700;
     private final int CANVAS_HEIGHT = 600; 
     private GraphicsGroup pieces;
-    private boolean player1Turn;  //Red if 1, Yellow if 2
     private GraphicsObject [][] ellipseGameBoard;
     private HashMap<Integer[][], GraphicsObject> tokenMap;
     private int turnCount;
-    private boolean gameOver;
+    private boolean gameIsOver;
     private static final int COLUMNS=7;
     private static final int ROWS=6;
     
     public GameManager() {
         turnCount = 0;
-        gameOver = false;
+        gameIsOver= false;
         board = new Board();
         canvas = new CanvasWindow("Board", CANVAS_WIDTH, CANVAS_HEIGHT);
         pieces = new GraphicsGroup();
         canvas.add(pieces, 0, 0);
         board.initializePieces(canvas);
         canvas.onClick(event -> {
-            int index = board.getNearestColIndex(event.getPosition().getX(), event.getPosition().getY());
-            System.out.println(index);
-            Fillable[] col = board.getGameBoard()[index];
-            if(turnCount < 43){
-                int count = 0;
-                while(count < 6){
-                    if(col[count].getFillColor() != Color.WHITE){
-                        break;
-                    }
-                    count ++;
-                }
-                board.getGameBoard()[index][count - 1].setFillColor(getPlayerColor(player1Turn));
-            }
-            canvas.draw();
-            checkWin(getPlayerColor(player1Turn));
-            player1Turn = !player1Turn;    
+            if(!gameIsOver) {
+                takeATurn(event.getPosition().getX(), event.getPosition().getY());
+            } else clearGame();
         });
-
     }
-    public Color getPlayerColor(boolean turn){
-        if(turn==true){
+
+    public void takeATurn(double x, double y){
+        int index = board.getNearestColIndex(x, y);
+        if(index != -1){
+            Fillable[] col = board.getGameBoard()[index];
+            int count = 0;
+            while(count < 6){
+                if(col[count].getFillColor() != Color.WHITE){
+                    break;
+                }
+                count ++;
+            }
+            if(count != 0) {
+                board.getGameBoard()[index][count - 1].setFillColor(getPlayerColor());
+                canvas.draw();
+                checkWin(getPlayerColor());
+                turnCount++;
+            }
+            if(turnCount == 41 && !gameIsOver) {
+                gameOver(false);
+            }
+        }
+    }
+
+    public void gameOver(boolean playerWon) {
+        if (playerWon) {
+            if (turnCount % 2 == 0) { // checks if player 1 wins because they are an even move number (first move made by red is move 0), not 1
+                System.out.println("Red Wins");
+            } else {
+                System.out.println("Yellow wins");
+            }
+        } 
+        if(turnCount == 41 && !playerWon){
+            System.out.println("Nobody is a winner");
+        }
+        gameIsOver = true;
+    }
+
+    public void clearGame() {
+        canvas.removeAll();
+        canvas.add(pieces);
+        turnCount = 0;
+        board.initializePieces(canvas);
+        gameIsOver = false;
+    }
+
+    public Color getPlayerColor(){
+        if(turnCount % 2 == 1){
             return Color.YELLOW;
         }
         else{
             return Color.RED;
         }
-
     }
+
     public Color getPieceColor(int column, int row){
-        if(column<COLUMNS &&row<ROWS){
-        return (Color) board.getGameBoard()[column][row].getFillColor();
+        if(column<COLUMNS && column > -1 && row < ROWS && row > -1){
+            return (Color) board.getGameBoard()[column][row].getFillColor();
         }
         return Color.WHITE;
     }
-    public boolean checkWin(Color color){
+
+    public void checkWin(Color color){
         // Checks rows for 4 in a row
-        for (int column=0; column<COLUMNS;column++){
-            for(int row=0;row<ROWS;row++){
-                if(getPieceColor(column, row)==color && getPieceColor(column+1, row)==color&& getPieceColor(column+2, row)==color&& getPieceColor(column+3, row)==color){
-                    System.out.println("Row Won");
-                    return true;
+        for (int column=0; column < COLUMNS; column++){
+            for(int row = 0; row < ROWS; row++){
+                if (getPieceColor(column, row)==color && 
+                    getPieceColor(column + 1, row)==color&& 
+                    getPieceColor(column+2, row)==color&& 
+                    getPieceColor(column+3, row)==color){
+                    gameOver(true);
                 }
                 // Checks columns for 4 in a row.
-                if(getPieceColor(column, row)==color && getPieceColor(column, row+1)==color&& getPieceColor(column, row+2)==color&& getPieceColor(column, row+3)==color){
-                    System.out.println("Column Won");
-                    return true;
+                if (getPieceColor(column, row)==color && getPieceColor(column, row + 1) == color && getPieceColor(column, row+2)==color&& getPieceColor(column, row+3)==color){
+                    gameOver(true);
                 }
                 // Checks Rightwards increasing diagonal for 4 in a row.
-                if(getPieceColor(column, row)==color && getPieceColor(column+1, row-1)==color&& getPieceColor(column+2, row-2)==color&& getPieceColor(column+3, row-3)==color){
-                    System.out.println("right diagonal Won");
-                    return true;
+                if (getPieceColor(column, row)==color &&
+                    getPieceColor(column+1, row-1)==color&&
+                    getPieceColor(column+2, row-2)==color&&
+                    getPieceColor(column+3, row-3)==color){
+                    gameOver(true);
                 }
                 //Checks leftward increasing diagonal for 4 in a row.
                 if(getPieceColor(column, row)==color && getPieceColor(column-1, row-1)==color&& getPieceColor(column-2, row-2)==color&& getPieceColor(column-3, row-3)==color){
-                    System.out.println("left diagonal Won");
-                    return true;
+                    gameOver(true);
                 }
             }  
         }
-        return false;
     }
 
     public void displayBoard(){
