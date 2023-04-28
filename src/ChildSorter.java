1c0e12003977efffbe4731de02a885a59a1ee9c2
James-J-McCarthy <112642209+James-J-McCarthy@users.noreply.github.com>
2023-04-28 14:20:35 -0500

    Update ChildSorter.java

--- src/ChildSorter.java
+++ src/ChildSorter.java
@@ -5,12 +5,17 @@ public class ChildSorter implements Comparator<Node> {
     public int compare(Node o1, Node o2) {
         if(o1.score > o2.score) {
             return 1;
-        } else if (o1.score < o2.score) {
+        } 
+        if (o1.score < o2.score) {
             return -1;
         }
         long previousMaskBit = o1.mask.bit & o2.mask.bit;
-        int positionPreferenceO1 = Long.numberOfTrailingZeros(o1.mask.bit ^ previousMaskBit) - 28;
-        int positionPreferenceO2 = Long.numberOfTrailingZeros(o2.mask.bit ^ previousMaskBit) - 28;
-        return positionPreferenceO1 - positionPreferenceO2;
+        int colO1 = Math.abs((Long.numberOfTrailingZeros(o1.mask.bit ^ previousMaskBit) / 7) - 3);
+        int colO2 = Math.abs((Long.numberOfTrailingZeros(o2.mask.bit ^ previousMaskBit) / 7 - 3));
+        if(colO1 < colO2){
+            return 1; 
+        } else {
+            return colO2;
+        }
     }
 }
