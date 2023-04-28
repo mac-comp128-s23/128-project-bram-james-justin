410ccb69e4ca247fee6d97c04282ba88e3e58472
James-J-McCarthy <112642209+James-J-McCarthy@users.noreply.github.com>
2023-04-28 14:46:01 -0500

    child sorter handles null

--- src/ChildSorter.java
+++ src/ChildSorter.java
@@ -3,19 +3,24 @@ import java.util.Comparator;
 public class ChildSorter implements Comparator<Node> {
     @Override
     public int compare(Node o1, Node o2) {
-        if(o1.score > o2.score) {
-            return 1;
-        } 
-        if (o1.score < o2.score) {
-            return -1;
-        }
-        long previousMaskBit = o1.mask.bit & o2.mask.bit;
-        int colO1 = Math.abs((Long.numberOfTrailingZeros(o1.mask.bit ^ previousMaskBit) / 7) - 3);
-        int colO2 = Math.abs((Long.numberOfTrailingZeros(o2.mask.bit ^ previousMaskBit) / 7 - 3));
-        if(colO1 < colO2){
-            return 1; 
+        if(o1 != null && o2 != null){
+            if(o1.score > o2.score) {
+                return 1;
+            } 
+            if (o1.score < o2.score) {
+                return -1;
+            }
+            long previousMaskBit = o1.mask.bit & o2.mask.bit;
+            int colO1 = Math.abs((Long.numberOfTrailingZeros(o1.mask.bit ^ previousMaskBit) / 7) - 3);
+            int colO2 = Math.abs((Long.numberOfTrailingZeros(o2.mask.bit ^ previousMaskBit) / 7 - 3));
+            if(colO1 < colO2){
+                return 1; 
+            } else {
+                return colO2;
+            }
         } else {
-            return colO2;
+            if(o1 != null) return -1;
+            return 1;
         }
     }
 }
